cb056d402f90892ac08551c83974a819e585f22a
Aman <amanmohadikar111@gmail.com>
2022-10-20 19:53:09 +0530

    Query PArams in post method

--- src/routes/route.js
+++ src/routes/route.js
@@ -145,8 +145,10 @@ router.post( "/post-query-1", function (req, res){
     res.send( {data: data , status: true})
 })
 
+
+
 //filter out all the numbers that are greater than "input" ( input is received from query params)
-let myArr = [23, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
+let myArr = [{aman: 23}, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
 
 router.post( "/post-query-2", function (req, res){
     let input= req.query.input
@@ -154,10 +156,59 @@ router.post( "/post-query-2", function (req, res){
     // let finArr= myArr.filter ( ele => ele>input )
     let finalArr= []
     for( i=0 ; i<myArr.length ; i++){
-        if ( myArr[i] > input )     finalArr.push( myArr[i])
+        if ( myArr[i] > input )     
+        finalArr.push( myArr[i])
     }
     res.send( {data: finalArr , status: true})
+ 
 })
+
+// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
+
+
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ router.post("/post-query-3",function(req,res){
+    let votingAge = req.query.votingAge
+    let final = [];
+    for(i = 0;i<persons.length;i++){
+
+        if(persons[i].age>votingAge)
+            final.push(persons[i]);
+            persons[i].votingStatus = true
+        }
+    res.send({data : final, status : true})
+})
+ 
+
+
 
 
 module.exports = router;
