32b976847e63a58d4bb5a91642b6ec3d8d8640be
Aman <amanmohadikar111@gmail.com>
2022-11-05 18:27:26 +0530

    completed

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,129 +1,129 @@
-const { count } = require("console")
+// const { count } = require("console")
 // const { isValidObjectId } = require("mongoose")
-const {ObjectId,model, isValidObjectId} =require("mongoose")
-const orderModel = require("../models/orderModel")
-const userIdModel = require("../models/userModel")
-const productModel = require("../models/productModel")
-
-
-const createOrder = async function (req, res) {
-    let { productId, userId } = req.body
-
-    if (!productId || !userId) {
-        return res.send({ msg: "userId and prodcutId is mandatory" })
-    }
-
-    if (!isValidObjectId(productId)) {
-        return res.send({ msg: "productId is not a valid objectId" })
-    }
-
-    if (!isValidObjectId(userId)) {
-        return res.send({ msg: "userIdId is not a valid objectId" })
-    }
-
-    let userIdDetails = await userIdModel.findById({ userId })
-    if (!userIdDetails) {
-        return res.send({ msg: "userId is not present" })
-    }
-
-
-    let productDetails = await productModel.findById({ productId })
-    if (!productDetails) {
-        return res.send({ msg: "productId is not present" })
-    }
-
-
-    const isFreeAppuserId = req.isFreeAppuserId
-    if (isFreeAppuserId) {
-        const order = await orderModel.create({
-            userId: userId,
-            productId: productId,
-            isFreeAppuserId: isFreeAppuserId,
-            date: new Date()
-        })
-        return res.send({ data: order })
-    } else {
-        if (userIdDetails.balance < productDetails.price) {
-            return res.send({ msg: "you dont  have sufficient balance" })
-        }
-    }
-
-    const orderDetails = {
-        userId: userId,
-        productId: productId,
-        amount: productDetails.price,
-        isFreeAppuserId: isFreeAppuserId,
-        date: date
-    }
-
-    const order = await orderModel.create(orderDetails)
-    const userIdId = await userIdModel.findByIdAndUpdate(userId, { inc: { $balance: -productDetails.price } })
-    return res.send({ data: order })
-}
-
-
-module.exports.createOrder = createOrder
-
-
-
-
-// const orderModel = require("../models/orderModel")
-// const productModel = require("../models/productModel")
-// const userModel = require("../models/userModel")
 // const {ObjectId,model, isValidObjectId} =require("mongoose")
+// const orderModel = require("../models/orderModel")
+// const userIdModel = require("../models/userModel")
+// const productModel = require("../models/productModel")
 
-// const createOrder= async function (req, res) {
-//     let userid=req.body.userId
-//     let productid=req.body.productId
-//     console.log(userid)
-//     console.log(productid)
+
+// const createOrder = async function (req, res) {
+//     let { productId, userId } = req.body
+
+//     if (!productId || !userId) {
+//         return res.send({ msg: "userId and prodcutId is mandatory" })
+//     }
+
+//     if (!isValidObjectId(productId)) {
+//         return res.send({ msg: "productId is not a valid objectId" })
+//     }
+
+//     if (!isValidObjectId(userId)) {
+//         return res.send({ msg: "userIdId is not a valid objectId" })
+//     }
+
+//     let userIdDetails = await userIdModel.findById({ userId })
+//     if (!userIdDetails) {
+//         return res.send({ msg: "userId is not present" })
+//     }
+
+
+//     let productDetails = await productModel.findById({ productId })
+//     if (!productDetails) {
+//         return res.send({ msg: "productId is not present" })
+//     }
+
+
+//     const isFreeAppuserId = req.isFreeAppuserId
+//     if (isFreeAppuserId) {
+//         const order = await orderModel.create({
+//             userId: userId,
+//             productId: productId,
+//             isFreeAppuserId: isFreeAppuserId,
+//             date: new Date()
+//         })
+//         return res.send({ data: order })
+//     } else {
+//         if (userIdDetails.balance < productDetails.price) {
+//             return res.send({ msg: "you dont  have sufficient balance" })
+//         }
+//     }
+
+//     const orderDetails = {
+//         userId: userId,
+//         productId: productId,
+//         amount: productDetails.price,
+//         isFreeAppuserId: isFreeAppuserId,
+//         date: date
+//     }
+
+//     const order = await orderModel.create(orderDetails)
+//     const userIdId = await userIdModel.findByIdAndUpdate(userId, { inc: { $balance: -productDetails.price } })
+//     return res.send({ data: order })
+// }
+
+
+// module.exports.createOrder = createOrder
+
+
+
+
+const orderModel = require("../models/orderModel")
+const productModel = require("../models/productModel")
+const userModel = require("../models/userModel")
+const {isValidObjectId} =require("mongoose")
+
+const createOrder= async function (req, res) {
+    let userid=req.body.userId
+    let productid=req.body.productId
+    console.log(userid)
+    console.log(productid)
     
 
-//     if(!isValidObjectId(userid)){
-//         return res.send("Invalid user Id ")
-//     }
+    if(!isValidObjectId(userid)){
+        return res.send("Invalid user Id ")
+    }
 
-//     if(!isValidObjectId(productid)){
-//         return res.send("Invalid product Id")
-//     }
-//     let checkproductid= await productModel.findById(productid)
-//     let Price = checkproductid.price
-//     let order=req.body
-//     console.log(Price)
+    if(!isValidObjectId(productid)){
+        return res.send("Invalid product Id")
+    }
+    let checkproductid= await productModel.findById(productid)
+    let Price = checkproductid.price
+    let order=req.body
+    console.log(Price)
 
-//     order.isFreeAppUser=isFreeAppUser
-//     if(order.isFreeAppUser==="true"){
-//        let result= order.amount=0
-//         console.log(result)
-//         let orderCreated = await orderModel.create(order)
-//         res.send({data: orderCreated})
-//     }
-//     else{
+    order.isFreeAppUser=isFreeAppUser
+    if(order.isFreeAppUser==="true"){
+       let result= order.amount=0
+        console.log(result)
+        let orderCreated = await orderModel.create(order)
+        res.send({data: orderCreated})
+    }
+    else{
     
 
  
-//     let checkuserid= await userModel.findById(userid)
-// let Balance = checkuserid.balance
+    let checkuserid= await userModel.findById(userid)
+let Balance = checkuserid.balance
     
    
-//     if(Price>=Balance){
-//         return res.send("Insufficient Balance")
-//     }
-//     let user1 = await userModel.findOneAndUpdate(
-//         {_id:userid},
-//         {$inc:{Balance:-Price}},
-//         { new: true}
-//     )
+    if(Price>=Balance){
+        return res.send("Insufficient Balance")
+    }
+    let user1 = await userModel.findOneAndUpdate(
+        {_id:userid},
+        {$inc:{Balance:-Price}},
+        { new: true}
+    )
    
-//         order.amount=Price
+        order.amount=Price
        
-//     let orderCr = await orderModel.create(order)
-//     res.send({msg:orderCr,data:user1})
-//     console.log(orderCr)
-// }
+    let orderCr = await orderModel.create(order)
+    res.send({msg:orderCr,data:user1})
+    console.log(orderCr)
+}
     
-// } 
-// module.exports.createOrder = createOrder
+} 
+module.exports.createOrder = createOrder
 
 
 
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -18,52 +18,36 @@ const mid3= function ( req, res, next) {
 const mid4= function ( req, res, next) {
     console.log("Hi I am a middleware named Mid4")
     next()
-}
-
-
-// const headerValidation = async function(req,res,next){               
-   
-//     let data=req.headers
-//     let validinfo=data["isfreeappuser"]
-//     if(validinfo){
-//         if(validinfo=="true"){
-//             req.isfreeappuser=true
-//         }else{
-//             req.isfreeappuser=false
-//         }
-//         next();
-//     }else{
-//         res.send({data:"header missing"})
-//     }
-//     }
+}   
 
 
 
-const headerValidation = function(req, res, next){
-
-    let isFreeAppUser = req.headers.isfreeappuser
-
-    if(!isFreeAppUser){
-        return res.send({msg : "mandatory is not present"})
-    }else{
-        isFreeAppUser = isFreeAppUser.toLowerCase() === "true" ? true:false
-        req.isFreeAppUser = isFreeAppUser
-        console.log("isFreeAppUser is added")
-        next()    
-    }
-}
 
 // const headerValidation = function(req, res, next){
 
-//      isFreeAppUser = req.headers.isfreeappuser
+//     let isFreeAppUser = req.headers.isfreeappuser
 
-//     if(isFreeAppUser){
-//         next()
-//     } else {
-//         res.send({msg:"header is mandatory"})
+//     if(!isFreeAppUser){
+//         return res.send({msg : "mandatory is not present"})
+//     }else{
+//         isFreeAppUser = isFreeAppUser.toLowerCase() === "true" ? true:false
+//         req.isFreeAppUser = isFreeAppUser
+//         console.log("isFreeAppUser is added")
+//         next()    
 //     }
-    
 // }
+
+const headerValidation = function(req, res, next){
+     isFreeAppUser = req.headers.isfreeappuser
+
+    if(isFreeAppUser){
+        next()
+    } else {
+        res.send({msg:"header is mandatory"})
+    }
+    
+}
+
 
 module.exports.mid1= mid1
 module.exports.mid2= mid2
