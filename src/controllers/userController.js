9b101727f8adf696a835f5e17221f2d326944e10
Aman <amanmohadikar111@gmail.com>
2022-11-07 19:46:17 +0530

    Completed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,44 +3,55 @@ const userModel = require("../models/userModel");
 const { route } = require("../routes/route");
 
 const createUser = async function (req, res) {
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  console.log(req.newAtribute);
-  res.send({ msg: savedData });
-};
+  try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    res.status(201).send({ msg: savedData });
+  }
+  catch (error) {
+    res.status(500).send({ status: false, msg: error.message })
+  }
+}
 
 
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.send({
+        status: false,
+        msg: "username or the password is not corerct",
+      });
 
-  // Once the login is successful, create the jwt token with sign function
-  // Sign function has 2 inputs:
-  // Input 1 is the payload or the object containing data to be set in token
-  // The decision about what data to put in token depends on the business requirement
-  // Input 2 is the secret
-  // The same secret will be used to decode tokens
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FUnctionUp",
-    },
-    "functionup-thorium"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
-};
+    // Once the login is successful, create the jwt token with sign function
+    // Sign function has 2 inputs:
+    // Input 1 is the payload or the object containing data to be set in token
+    // The decision about what data to put in token depends on the business requirement
+    // Input 2 is the secret
+    // The same secret will be used to decode tokens
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "thorium",
+        organisation: "FUnctionUp",
+      },
+      "functionup-thorium"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(201).send({ status: true, data: token });
+  }
+  catch (error) {
+    res.status(500).send({ status: false, error: error.message })
+  }
+}
+
+
 
 const getUserData = async function (req, res) {
-  token = req.headers["x-auth-token"];
+  try{  token = req.headers["x-auth-token"];
 
   //If no token is present in the request header return error
   if (!token) return res.send({ status: false, msg: "token must be present" });
@@ -53,6 +64,7 @@ const getUserData = async function (req, res) {
   // Input 2 is the same secret with which the token was generated
   // Check the value of the decoded token yourself
   let decodedToken = jwt.verify(token, "functionup-thorium");
+  console.log(decodedToken)
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
 
@@ -61,8 +73,15 @@ const getUserData = async function (req, res) {
   if (!userDetails)
     return res.send({ status: false, msg: "No such user exists" });
 
-  res.send({ status: true, data: userDetails });
-};
+  res.status(201).send({ status: true, data: userDetails });
+}
+catch(error){
+  res.status(500).send({status : false, msg: error.message})
+}
+}
+
+
+
 
 const updateUser = async function (req, res) {
   // Do the same steps here:
